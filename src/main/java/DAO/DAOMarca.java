cc72cdc13035919a639d06ebed300789499d5970
TakiClassic <luciano.360@hotmail.com>
2022-01-11 14:14:49 -0600

    Marca

---
+++ src/main/java/DAO/DAOMarca.java
@@ -0,0 +1,8 @@
+package DAO;
+
+public interface DAOMarca {
+    public void registrar(Marca mar) throws Exception;
+    public void listar(Marca mar) throws Exception;
+    public void modificar(Marca mar) throws Exception;
+    public void eliminar(Marca mar) throws Exception;      
+}
