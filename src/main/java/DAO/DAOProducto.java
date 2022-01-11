8102eb8ee9021322f98e1ae54f34ea1c2972499e
TakiClassic <luciano.360@hotmail.com>
2022-01-11 14:35:45 -0600

    Producto

---
+++ src/main/java/DAO/DAOProducto.java
@@ -0,0 +1,8 @@
+package DAO;
+
+public interface DAOProducto {
+    public void registrar(Producto pro) throws Exception;
+    public void listar(Producto pro) throws Exception;
+    public void modificar(Producto pro) throws Exception;
+    public void eliminar(Producto pro) throws Exception;
+}
